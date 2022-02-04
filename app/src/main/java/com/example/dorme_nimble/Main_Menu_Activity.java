ee78450dadd2c9364f0956a0fef06674ce6d9799
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2022-02-05 01:00:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dorme_nimble/Main_Menu_Activity.java
@@ -0,0 +1,14 @@
+package com.example.dorme_nimble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_Menu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
